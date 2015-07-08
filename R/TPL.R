fca0b7b83e65aa23861d25b78e38496ba0818cbf
Jinlong Zhang <jinlongzhang01@gmail.com>
2015-07-08 23:59:57 +0800

    Fixed the query bug by family names

--- R/TPL.R
+++ R/TPL.R
@@ -34,12 +34,15 @@ TPL <- function(plant.names = NULL){
     genus <- data.frame(YOUR_SPECIES = plant.names, YOUR_GENUS = genus)
     res0 <- merge(x = genus, y = kew.db, by = "YOUR_GENUS", by.y = "QUERY", sort = FALSE, all.x = TRUE)
     
-    res1 <- merge(x = res0, y = plantlist.db, by.x = "KEW_GENUS_EXT", by.y = "GENUS", sort = FALSE, all.x = TRUE)
+    res1 <- merge(x = res0, y = plantlist.db, by.x = "YOUR_GENUS", by.y = "GENUS", sort = FALSE, all.x = TRUE)
 
     tpl.orders.path <- system.file("extdata", "APG_ORDERS.csv", package = "plantlist")
     tpl.orders <- read.csv(tpl.orders.path, header = TRUE, stringsAsFactors = FALSE)
     res <- merge(x = res1, y = tpl.orders, by = "TPL_FAMILY", sort = FALSE, all.x = TRUE)
-    return(data.frame(YOUR_SPECIES = res$YOUR_SPECIES, YOUR_GENUS = res$YOUR_GENUS, TPL_STATUS = res$TPL_STATUS, TPL_FAMILY = res$TPL_FAMILY, TPL_ORDER = res$TPL_ORDER, APGIII_NUMBER = res$APGIII_NUMBER, KEW_GENUS_EXT = res$KEW_GENUS_EXT, KEW_FAMILY_EXT = res$KEW_FAMILY_EXT, TPL_URL = res$TPL_URL))
+    return(data.frame(YOUR_SEARCH = res$YOUR_SPECIES, POSSIBLE_GENUS = res$YOUR_GENUS, 
+           TPL_STATUS = res$TPL_STATUS, TPL_FAMILY = res$TPL_FAMILY, TPL_ORDER = res$TPL_ORDER, 
+           APGIII_NUMBER = res$APGIII_NUMBER, KEW_GENUS_EXT = res$KEW_GENUS_EXT, 
+           KEW_FAMILY_EXT = res$KEW_FAMILY_EXT, TPL_URL = res$TPL_URL))
 }
 
 
